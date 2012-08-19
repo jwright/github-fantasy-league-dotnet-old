3919a44bfb83050049104dd965dedaad402b02b4
Jamie Wright <jamie@brilliantfantastic.com>
2012-08-18 23:53:08 -0400

    Added SpecFlow project, scenario, and step definitions.

---
+++ GithubFantasyLeague.Specs/StepDefinitions/GitHubSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace GitHubFantasyLeauge.Specs.StepDefinitions
+{
+    [Binding]
+    public class GitHubSteps
+    {
+        [Given(@"I have a valid github username")]
+        public void GivenIHaveAValidGithubUsername()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I enter the Github username")]
+        public void WhenIEnterTheGithubUsername()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should see the total score")]
+        public void ThenIShouldSeeTheTotalScore()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
---
+++ GithubFantasyLeague.Specs/StepDefinitions/NavigationSteps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace GitHubFantasyLeauge.Specs.StepDefinitions
+{
+    [Binding]
+    public class NavigationSteps
+    {
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
